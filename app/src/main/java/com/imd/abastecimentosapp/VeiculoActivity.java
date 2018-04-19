38e48325910ec561c78243caac0c9a6638f94ac4
Davi <davimalta10@gmail.com>
2018-04-19 17:04:20 -0300

    Primeiro commit - Estrutura base do projeto com as Activity e menu

---
+++ app/src/main/java/com/imd/abastecimentosapp/VeiculoActivity.java
@@ -0,0 +1,35 @@
+package com.imd.abastecimentosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class VeiculoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veiculo);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getSupportActionBar().setTitle("Veículo - Cadastro");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_enter, R.anim.slide_exit);
+    }
+}
